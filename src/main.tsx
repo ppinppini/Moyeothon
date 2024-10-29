e7727d74a581b1dd87fbcb6717dc1cb360bac7d2
황유빈 <bnm0218@naver.com>
2024-10-29 22:29:49 +0900

    Fix: 스트릭트 모드 해제

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import { RouterProvider } from 'react-router-dom';
@@ -6,11 +5,9 @@ import { router } from './router/index.tsx';
 import { ReactQueryProvider } from './config/ReactQueryProvider.tsx';
 import { ThemeProvider } from '@material-tailwind/react';
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <ThemeProvider>
-      <ReactQueryProvider>
-        <RouterProvider router={router} />
-      </ReactQueryProvider>
-    </ThemeProvider>
-  </StrictMode>,
+  <ThemeProvider>
+    <ReactQueryProvider>
+      <RouterProvider router={router} />
+    </ReactQueryProvider>
+  </ThemeProvider>,
 );
