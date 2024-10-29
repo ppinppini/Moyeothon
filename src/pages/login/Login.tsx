92625e0d2dd4e408cde81938e494aac2b22c2225
황유빈 <bnm0218@naver.com>
2024-10-30 00:57:11 +0900

    Style:로그인 페이지 스타일링

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,5 +1,7 @@
-import kakaoLoginBtn from '../../assets/kakao_login_btn.png';
+import kakaoLoginBtn from '../../assets/kakao_login_medium_narrow.png';
+import kakaoLoginBtn1 from '../../assets/kakao_login_btn.svg';
 import googleLoginBtn from '../../assets/google_login_btn.png';
+import logo from '../../assets/logo.png';
 const K_REST_API_KEY = import.meta.env.VITE_APP_REST_API_KEY;
 const K_REDIRECT_URI = import.meta.env.VITE_APP_REDIRECT_URI;
 const kakaoURL = `https://kauth.kakao.com/oauth/authorize?client_id=${K_REST_API_KEY}&redirect_uri=${K_REDIRECT_URI}&response_type=code`;
@@ -15,21 +17,44 @@ const Login = () => {
   const kakaoLogin = () => {
     window.location.href = kakaoURL;
   };
-  //구글 로그인 함수 
+  //구글 로그인 함수
   const googleLogin = () => {
     window.location.href = googleURL;
   };
 
   return (
-    <>
-      <h1>Login Component</h1>
-      <div className="cursor-pointer" onClick={kakaoLogin}>
-        <img src={kakaoLoginBtn} alt="카카오 로그인 버튼" />
+    <div className="bg-gradient-to-b from-[#88A5D5] to-white h-screen flex flex-col justify-center items-center gap-4 ">
+      <div>
+        <img src={logo} alt="로고이미지" />
+      </div>
+      {/* <div
+        className="cursor-pointer bg-[#FEE500] font-bold rounded-3xl"
+        onClick={kakaoLogin}
+      >
+        <img
+          className="w-[300px]"
+          src={kakaoLoginBtn}
+          alt="카카오 로그인 버튼"
+        />
+      </div> */}
+      <div
+        className="cursor-pointer bg-[#FEE500] font-bold rounded-3xl"
+        onClick={kakaoLogin}
+      >
+        <img
+          className="w-[300px]"
+          src={kakaoLoginBtn1}
+          alt="카카오 로그인 버튼"
+        />
       </div>
       <div className="cursor-pointer" onClick={googleLogin}>
-        <img src={googleLoginBtn} alt="구글 로그인 버튼" />
+        <img
+          src={googleLoginBtn}
+          className="w-[300px]"
+          alt="구글 로그인 버튼"
+        />
       </div>
-    </>
+    </div>
   );
 };
 export default Login;
