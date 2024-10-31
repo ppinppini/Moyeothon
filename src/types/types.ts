709f65f418df216fc00fd01a193adf9b6779146e
황유빈 <bnm0218@naver.com>
2024-11-01 02:06:40 +0900

    Feat:사용자 타입 선언

--- src/types/types.ts
+++ src/types/types.ts
@@ -3,3 +3,29 @@ export interface User {
   name: string;
   password: string;
 }
+
+// 유저간 송수신 인터페이스 정의
+export interface IUser {
+  id: number;
+  uid: string;
+  password: string;
+  name: string;
+  nickname: string;
+  email: string;
+  provider: string;
+}
+
+export interface IMessage {
+  id: number;
+  content: string;
+  createTime: string;
+  senderId: number;
+  receiverId: number;
+  sender: User;
+  receiver: User;
+  status: string;
+}
+
+export interface IUsers {
+  messages: IMessage[];
+}
