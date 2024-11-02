9b9f5fabdb8dcfb0deacae5a68a607b34222d324
황유빈 <bnm0218@naver.com>
2024-11-02 21:28:21 +0900

    Chore: api 코드

--- src/pages/login/KRedirect.tsx
+++ src/pages/login/KRedirect.tsx
@@ -1,6 +1,7 @@
 import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Loading from '../../components/Loading';
+import { apiClient } from '../../api/api';
 
 const KRedirect = () => {
   const navigate = useNavigate();
@@ -11,24 +12,19 @@ const KRedirect = () => {
     // console.log('Authorization code:', code);
 
     const fetchToken = async (code: string) => {
-      if (!code) return;
-      const response = await fetch(
-        `https://newteamsgoody.shop/user/oauth2/code/kakao?code=${code}`
-
-        // {
-        //   method: 'POST',
-        //   headers: {
-        //     'Content-Type': 'application/json',
-        //   },
-        //   body: JSON.stringify({ code }),
-        // },
-      );
-      const data = await response.json();
-      // console.log(data);
-      localStorage.setItem('token', data.token);
-      localStorage.setItem('uid', data.user.uid);
-      // localStorage.setItem('userId', data.user.id);
-      navigate('/');
+      try {
+        if (!code) return;
+        const response = await apiClient.get(
+          `/user/oauth2/code/kakao?code=${code}`
+        );
+        const data = response.data;
+        localStorage.setItem('token', data.token);
+        localStorage.setItem('uid', data.user.uid);
+        navigate('/');
+      } catch (error) {
+        console.error('Error fetching token:', error);
+        // 에러 처리
+      }
     };
 
     if (code) {
