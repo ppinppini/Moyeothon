fbaa18148ace39c7c36f268d7c6b8bc7abfe31ed
황유빈 <bnm0218@naver.com>
2024-11-02 21:05:41 +0900

    배포 설정

--- src/pages/login/KRedirect.tsx
+++ src/pages/login/KRedirect.tsx
@@ -13,7 +13,7 @@ const KRedirect = () => {
     const fetchToken = async (code: string) => {
       if (!code) return;
       const response = await fetch(
-        `https://newteamsgoody.shop/user/oauth2/code/kakao?code=${code}`,
+        `https://newteamsgoody.shop/user/oauth2/code/kakao?code=${code}`
 
         // {
         //   method: 'POST',
