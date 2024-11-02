a1366e0bd4a27e1243f9529afc8f4291c8b38eda
황유빈 <bnm0218@naver.com>
2024-11-02 18:30:05 +0900

    Chore:타입 추가

--- src/pages/login/KRedirect.tsx
+++ src/pages/login/KRedirect.tsx
@@ -5,11 +5,11 @@ import Loading from '../../components/Loading';
 const KRedirect = () => {
   const navigate = useNavigate();
   // console.log('Authorization code:', code);
-  
+
   useEffect(() => {
     const code = new URL(window.location.href).searchParams.get('code');
     console.log('Authorization code:', code);
-    
+
     // const fetchToken = async (code: string) => {
     //   if (!code) return;
     //   const response = await fetch(`/api/user/oauth2/code/kakao?code=${code}`, {
@@ -26,26 +26,29 @@ const KRedirect = () => {
     //   // localStorage.setItem('userId', data.user.id);
     //   navigate('/home');
     // };
-    const fetchToken = async (code) => {
+    const fetchToken = async (code: string) => {
       if (!code) return;
-    
+
       try {
-        const response = await fetch(`/api/user/oauth2/code/kakao?code=${code}`, {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
+        const response = await fetch(
+          `/api/user/oauth2/code/kakao?code=${code}`,
+          {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ code }),
           },
-          body: JSON.stringify({ code }),
-        });
-    
+        );
+
         if (!response.ok) {
           console.error('Error:', response.status);
           throw new Error('Failed to fetch');
         }
-    
+
         // Content-Type이 JSON인지 확인
-        const contentType = response.headers.get("Content-Type");
-        if (contentType && contentType.includes("application/json")) {
+        const contentType = response.headers.get('Content-Type');
+        if (contentType && contentType.includes('application/json')) {
           const data = await response.json();
           localStorage.setItem('token', data.token);
           localStorage.setItem('uid', data.user.uid);
@@ -53,14 +56,13 @@ const KRedirect = () => {
         } else {
           // JSON이 아닐 경우 오류 처리
           const text = await response.text();
-          console.error("Response is not JSON:", text);
-          throw new Error("Received non-JSON response");
+          console.error('Response is not JSON:', text);
+          throw new Error('Received non-JSON response');
         }
       } catch (error) {
-        console.error("Error fetching token:", error);
+        console.error('Error fetching token:', error);
       }
     };
-    
 
     if (code) {
       fetchToken(code);
